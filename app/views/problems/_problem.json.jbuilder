20fc970ba3cb1823427bb192c596fa74cd5695e1
Dmitry B <barskoidim@gmail.com>
2019-09-26 14:59:53 +0300

    add model Problem

---
+++ app/views/problems/_problem.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! problem, :id, :title, :description, :user_id, :created_at, :updated_at
+json.url problem_url(problem, format: :json)
---
+++ db/migrate/20190926115930_create_problems.rb
@@ -0,0 +1,11 @@
+class CreateProblems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :problems do |t|
+      t.string :title
+      t.string :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
