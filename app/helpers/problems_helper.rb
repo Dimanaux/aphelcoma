5ca51841563bcd11744d7530eec1a5719df7b4f3
Dmitry B <barskoidim@gmail.com>
2019-10-07 18:21:03 +0300

    set up markdown, add code blocks

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,12 +1,9 @@
 module ProblemsHelper
   def markdown(text)
-    # options = {
-    #   :fenced_code_blocks => true,
-    #   :no_intra_emphasis => true,
-    #   :autolink => true,
-    #   :lax_html_blocks => true,
-    # }
-    markdown_to_html = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    options = {
+      fenced_code_blocks: true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
     markdown_to_html.render(text).html_safe
   end
 end
