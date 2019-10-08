1f589d8cde540846f483e36f1453eef6f04ce5ef
Dmitry B <barskoidim@gmail.com>
2019-10-08 08:54:02 +0300

    remove json responses

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -21,10 +21,8 @@ class ProblemsController < ApplicationController
     respond_to do |format|
       if @problem.save
         format.html { redirect_to @problem, notice: "Problem was successfully created." }
-        format.json { render :show, status: :created, location: @problem }
       else
         format.html { render :new }
-        format.json { render json: @problem.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -33,10 +31,8 @@ class ProblemsController < ApplicationController
     respond_to do |format|
       if @problem.update(problem_params)
         format.html { redirect_to @problem, notice: "Problem was successfully updated." }
-        format.json { render :show, status: :ok, location: @problem }
       else
         format.html { render :edit }
-        format.json { render json: @problem.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -45,11 +41,11 @@ class ProblemsController < ApplicationController
     @problem.destroy
     respond_to do |format|
       format.html { redirect_to problems_url, notice: "Problem was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
+
   def set_problem
     @problem = Problem.find(params[:id])
   end
