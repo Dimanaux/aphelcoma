d97fb3e2ce890f295c074c582f79428ab58be51d
Dmitry B <barskoidim@gmail.com>
2019-10-08 08:52:45 +0300

    remove generated comments

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -2,26 +2,18 @@ class ProblemsController < ApplicationController
   before_action :set_problem, only: %I[show edit update destroy]
   before_action :authenticate_user!, only: %I[new edit create destroy]
 
-  # GET /problems
-  # GET /problems.json
   def index
     @problems = Problem.all
   end
 
-  # GET /problems/1
-  # GET /problems/1.json
   def show; end
 
-  # GET /problems/new
   def new
     @problem = Problem.new
   end
 
-  # GET /problems/1/edit
   def edit; end
 
-  # POST /problems
-  # POST /problems.json
   def create
     @problem = Problem.new(problem_params)
     @problem.user = current_user
@@ -37,8 +29,6 @@ class ProblemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /problems/1
-  # PATCH/PUT /problems/1.json
   def update
     respond_to do |format|
       if @problem.update(problem_params)
@@ -51,8 +41,6 @@ class ProblemsController < ApplicationController
     end
   end
 
-  # DELETE /problems/1
-  # DELETE /problems/1.json
   def destroy
     @problem.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class ProblemsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_problem
     @problem = Problem.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def problem_params
     params.require(:problem).permit(:title, :description, :user_id)
   end
