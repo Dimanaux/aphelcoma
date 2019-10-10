9952002f2a0666095d017b7e04dea532b77ec22e
Dmitry B <barskoidim@gmail.com>
2019-10-10 14:14:54 +0300

    simplify routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show", as: "user"
   resources :problems
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %I[index show]
   root to: "problems#index"
 end
