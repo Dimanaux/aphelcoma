a90d3996f8a59bcebc0ef9be0216a14771679ede
Dmitry Barskov <barskoidim@gmail.com>
2019-10-20 20:47:15 +0300

    create comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,43 @@
+class CommentsController < ApplicationController
+  before_action :set_problem
+  before_action :set_comment, only: [:update, :destroy]
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.problem = @problem
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @problem, notice: 'Comment was successfully created.'
+    else
+      redirect_to @problem
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @problem, notice: 'Comment was successfully updated.'
+    else
+      redirect_to @problem
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @problem, notice: 'Comment was successfully destroyed.'
+  end
+
+  private
+
+  def set_problem
+    @problem = Problem.find(params[:problem_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:problem_id, :user_id, :text)
+  end
+end
