582d907a6cca7168135ea4983ac660858f63d021
Dmitry Barskov <barskoidim@gmail.com>
2019-10-20 19:18:01 +0300

    bind comment to user, problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  PER_PAGE = 24
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PER_PAGE = 24
   paginates_per PER_PAGE
 
   has_many :problems, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
