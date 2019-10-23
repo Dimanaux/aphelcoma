5eed22d10e46175b369d60553ccf9d81cad8a750
Dmitry Barskov <barskoidim@gmail.com>
2019-10-23 07:13:02 +0300

    remove trailing whitespaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: %I[index show], param: :username  
+  resources :users, only: %I[index show], param: :username
   resources :problems do
     resources :comments, only: %I[create update destroy]
   end
