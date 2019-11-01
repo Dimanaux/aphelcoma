ebf71eeea4084c41c34f930f318d56c88df28947
Dmitry B <barskoidim@gmail.com>
2019-11-01 09:54:11 +0300

    set up route for oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %I[index show], param: :username
+
   resources :problems do
     resources :comments, only: %I[create update destroy]
   end
+
+  # Routes for Google authentication
+  get "auth/:provider/callback", to: "sessions#googleAuth", as: "user_google_omniauth_authorize"
+
   root to: "problems#index"
 end
