fc3aae1d29465fb44f46680ad7c124485bf0f200
Dmitry Barskov <barskoidim@gmail.com>
2019-11-02 12:11:43 +0300

    add authorization error rescue in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,18 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not allowed to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 end
