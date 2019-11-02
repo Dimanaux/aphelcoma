5fa162d44c9865336d87a54d2fd3ec32e48b3e61
Dmitry Barskov <barskoidim@gmail.com>
2019-11-03 00:59:31 +0300

    remove manually created oauth route, add oauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,21 +1,19 @@
-# class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-#   def google
-#     - byebug
-#     # Get access tokens from the google server
-#     access_token = request.env["omniauth.auth"]
-#     user = User.from_omniauth(access_token)
-#     log_in(user)
-#     # Access_token is used to authenticate request made from the rails application to the google server
-#     user.google_token = access_token.credentials.token
-#     # Refresh_token to request new access_token
-#     # Note: Refresh_token is only sent once during the first request
-#     refresh_token = access_token.credentials.refresh_token
-#     user.google_refresh_token = refresh_token if refresh_token.present?
-#     user.save
-#     redirect_to root_path
-#   end
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def google_oauth2
+      auth_data = request.env["omniauth.auth"]
+      @user = User.from_omniauth(auth_data)
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
+        sign_in_and_redirect @user, event: :authentication
+      else
+        session["devise.google_data"] = auth_data.except(:extra)
+        redirect_to new_user_registration_url, flash: @user.errors
+      end
+    end
 
-#   def failure
-#     redirect_to root_path
-#   end
-# end
+    def failure
+      redirect_to root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   resources :users, only: %I[index show], param: :username
 
   resources :problems do
     resources :comments, only: %I[create update destroy]
   end
-
-  # Routes for Google authentication
-  get "auth/:provider/callback", to: "sessions#googleAuth", as: "user_google_omniauth_authorize"
 
   root to: "problems#index"
 end
