f31e1eb1336f6a91145801f0f61c90fcce508d2c
Dmitry B <barskovdmitry42@gmail.com>
2019-11-14 14:52:24 +0300

    Add tests for comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,7 +1,12 @@
 require "rails_helper"
 
 describe Comment, type: :model do
+  context "validations" do
+    it { should validate_presence_of :text }
+  end
+
   context "associations" do
     it { should belong_to :user }
+    it { should belong_to :problem }
   end
 end
