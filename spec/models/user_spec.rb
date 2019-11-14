43d238d4d26f907da5227d38efc430628a169145
Dmitry B <barskovdmitry42@gmail.com>
2019-11-14 21:26:45 +0300

    Add test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 describe User, type: :model do
   context "validations" do
     it { should validate_presence_of :username }
+    it { should validate_uniqueness_of :username }
   end
 
   context "associations" do
