531421b7d391899ed9f75ff0db320b2998b8ee4e
Dmitry B <barskovdmitry42@gmail.com>
2019-11-18 23:37:25 +0300

    fix styles in spec/support/capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,13 +6,16 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { "w3c" => false, args: %w[disable-gpu window-size=1920,1080] })
+    chromeOptions: { "w3c" => false, args: %w[disable-gpu window-size=1920,1080] }
+  )
   client = Selenium::WebDriver::Remote::Http::Default.new
 
-  Capybara::Selenium::Driver.new app,
+  Capybara::Selenium::Driver.new(
+    app,
     browser: :chrome,
     http_client: client,
     desired_capabilities: capabilities
+  )
 end
 
 Capybara.javascript_driver = :headless_chrome
