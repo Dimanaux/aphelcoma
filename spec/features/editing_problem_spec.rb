d017d4d14f3fabc864fa1bd3f197e5c5f6394334
Dmitry B <barskovdmitry42@gmail.com>
2019-11-18 22:58:05 +0300

    remove destruction functionality from editing_problem_spec

--- spec/features/editing_problem_spec.rb
+++ spec/features/editing_problem_spec.rb
@@ -34,19 +34,4 @@ describe "Editing problem" do
     visit problem_path(problem)
     expect(page).not_to have_content "Edit"
   end
-
-  # TODO: Add driver that supports modals
-  # it "allows to destroy my problem" do
-  #   visit problem_path(my_problem)
-  #   expect(page).to have_content "Destroy"
-  #   click_link "Destroy"
-  #   page.accept_alert
-  #   expect(page).to have_content "Problem was sucessfully destroyed"
-  #   expect(page).not_to have_content my_problem.title
-  # end
-
-  it "forbids to destroy other user's problem" do
-    visit problem_path(problem)
-    expect(page).not_to have_content "Destroy"
-  end
 end
