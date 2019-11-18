0bc2bca1b3a55cdb7f766b1d440c87ec71a0698d
Dmitry B <barskovdmitry42@gmail.com>
2019-11-18 22:46:19 +0300

    Refactor spec config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,8 +17,3 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-
-RSpec.configure do |config|
-  config.infer_spec_type_from_file_location!
-  config.filter_rails_from_backtrace!
-end
--- spec/support/rspec.rb
+++ spec/support/rspec.rb
@@ -4,8 +4,6 @@ RSpec.configure do |config|
     expectations.syntax = :expect
   end
 
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-    expectations.syntax = :expect
-  end
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
 end
