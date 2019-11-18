9a8a156d117debd6a08b9b537a44a8fc8315bae1
Dmitry B <barskovdmitry42@gmail.com>
2019-11-18 22:51:02 +0300

    Add spec for problem edit

---
+++ spec/features/editing_problem_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+describe "Editing problem" do
+  include_context "logged in"
+
+  let(:my_problem) { create(:problem, user: current_user) }
+  let(:problem) { create(:problem) }
+
+  it "allows to edit my problem" do
+    visit problem_path(my_problem)
+    expect(page).to have_content "Edit"
+  end
+
+  it "edits my problem" do
+    visit problem_path(my_problem)
+    new_title = "New problem's title"
+    click_link "Edit"
+    fill_in :problem_title, with: new_title
+    click_button "Update Problem"
+    expect(page).to have_content "Problem was successfully updated"
+    expect(page).to have_content new_title
+  end
+
+  it "forbids empty field on edit" do
+    visit problem_path(my_problem)
+    click_link "Edit"
+    fill_in :problem_title, with: ""
+    click_button "Update Problem"
+    expect(page).to have_content "Title can't be blank"
+    expect(page).not_to have_content "Problem was successfully updated"
+  end
+
+  it "forbids to edit other user's problem" do
+    visit problem_path(problem)
+    expect(page).not_to have_content "Edit"
+  end
+
+  # TODO: Add driver that supports modals
+  # it "allows to destroy my problem" do
+  #   visit problem_path(my_problem)
+  #   expect(page).to have_content "Destroy"
+  #   click_link "Destroy"
+  #   page.accept_alert
+  #   expect(page).to have_content "Problem was sucessfully destroyed"
+  #   expect(page).not_to have_content my_problem.title
+  # end
+
+  it "forbids to destroy other user's problem" do
+    visit problem_path(problem)
+    expect(page).not_to have_content "Destroy"
+  end
+end
