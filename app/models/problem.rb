880f63049867881520c15cac858eb88d8e65897d
Dmitry Barskov <barskoidim@gmail.com>
2019-11-20 18:23:01 +0300

    Add problem validation

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,6 +1,10 @@
 class Problem < ApplicationRecord
   PER_PAGE = 12
   paginates_per PER_PAGE
+
+  validates :title, presence: true
+  validates :description, presence: true
+
   belongs_to :user
   has_many :comments, dependent: :destroy
 end
