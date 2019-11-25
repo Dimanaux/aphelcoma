57672179cd68a1ee2816dda67b2a376494e5e147
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-11-25 20:33:01 +0300

    Delete problems helper

--- app/helpers/problems_helper.rb
+++
@@ -1,9 +0,0 @@
-module ProblemsHelper
-  def markdown(text)
-    options = {
-      fenced_code_blocks: true
-    }
-    markdown_to_html = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-    markdown_to_html.render(text).html_safe
-  end
-end
