9301dba9adee7fb216b250bdd53e34bb6c231829
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-11-27 18:50:09 +0300

    Remove accident files

--- spec/factories/views.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :view do
-    association :user, factory: :user
-    association :problem, factory: :problem
-  end
-end
--- spec/models/view_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-describe View do
-  context "with associations" do
-    it { is_expected.to belong_to :user }
-    it { is_expected.to belong_to :problem }
-  end
-end
