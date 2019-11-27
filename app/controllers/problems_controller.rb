e18f9a1048b2390e9177557a933ccff6a93656d2
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-11-27 20:53:28 +0300

    Use exposure for problems

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,5 +1,6 @@
 class ProblemsController < ApplicationController
-  before_action :set_problem, only: %I[show edit update destroy]
+  expose :problem
+  before_action :authorize_problem, only: %I[show new edit update destroy]
   before_action :authenticate_user!, only: %I[new edit create destroy]
 
   def index
@@ -7,39 +8,34 @@ class ProblemsController < ApplicationController
   end
 
   def show
-    @comments = @problem.comments.order(:created_at)
-    Views::Create.call(user: current_user, problem: @problem)
+    Views::Create.call(user: current_user, problem: problem)
   end
 
-  def new
-    @problem = Problem.new
-    authorize @problem
-  end
+  def new; end
 
   def edit; end
 
   def create
-    @problem = Problem.new problem_params.merge(user_id: current_user.id)
-    authorize @problem
-    @problem.save
-    respond_with @problem
+    problem.user = current_user
+    authorize problem
+    problem.save
+    respond_with problem
   end
 
   def update
-    @problem.update(problem_params)
-    respond_with @problem
+    problem.update(problem_params)
+    respond_with problem
   end
 
   def destroy
-    @problem.destroy
-    respond_with @problem
+    problem.destroy
+    respond_with problem
   end
 
   private
 
-  def set_problem
-    @problem = Problem.find(params[:id])
-    authorize @problem
+  def authorize_problem
+    authorize problem
   end
 
   def problem_params
