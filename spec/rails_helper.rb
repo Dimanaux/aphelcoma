dbf2c00f4872fe5bff2afa8a187dacdcb778e782
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-11-27 23:17:14 +0300

    Fix coverage meter: simplecov supposed to start at the very beginning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require "simplecov"
+SimpleCov.start
+
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 
@@ -6,7 +9,6 @@ require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 require "shoulda/matchers"
-require "simplecov"
 require "spec_helper"
 
 Dir[Rails.root.join("spec", "support", "**/*.rb")].each { |f| require f }
