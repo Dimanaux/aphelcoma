9df35f4310711830696eb577f5040a4f963f2f82
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-11-27 23:07:23 +0300

    add check for views association in User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,6 @@ describe User do
     it { is_expected.to have_many :problems }
     it { is_expected.to have_many :comments }
     it { is_expected.to have_many :solutions }
+    it { is_expected.to have_many :views }
   end
 end
