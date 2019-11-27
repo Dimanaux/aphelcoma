f343dff199a7960bd7c70c7c3f543e17ffa2132c
Dmitry Barskov <barskovdmitry42@gmail.com>
2019-11-27 21:25:29 +0300

    Apply exposure to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,34 +1,34 @@
 class CommentsController < ApplicationController
-  before_action :set_problem
-  before_action :set_comment, only: %I[edit update destroy]
+  expose :problem, id: :problem_id
+  expose :comment
+  before_action :authorize_comment, only: %I[update destroy]
 
   def edit; end
 
   def create
-    context = Comments::Create.call comment_params.merge(user: current_user, problem: @problem)
-    @comment = context.comment
-    respond_with @comment, location: -> { problem_path(@problem) }
+    context = Comments::Create.call comment_params.merge(user: current_user, problem: problem)
+    comment = context.comment
+    respond_with comment, problem_location
   end
 
   def update
-    @comment.update(comment_params)
-    respond_with @comment, location: -> { problem_path(@problem) }
+    comment.update(comment_params)
+    respond_with comment, problem_location
   end
 
   def destroy
-    @comment.destroy
-    respond_with @comment, location: -> { problem_path(@problem) }
+    comment.destroy
+    respond_with comment, problem_location
   end
 
   private
 
-  def set_problem
-    @problem = Problem.find(params[:problem_id])
+  def problem_location
+    { location: -> { problem_path(problem) } }
   end
 
-  def set_comment
-    @comment = Comment.find(params[:id])
-    authorize @comment
+  def authorize_comment
+    authorize comment
   end
 
   def comment_params
