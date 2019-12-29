f7a9d6402089e63c3be062a5fc16214e8e82590a
Dmitry Barskov <barskoidim@gmail.com>
2019-12-29 17:20:16 +0300

    Remove unused code from Comments::Create

--- app/interactors/comments/create.rb
+++ app/interactors/comments/create.rb
@@ -1,7 +1,6 @@
 module Comments
   class Create
     include Interactor
-    include Pundit
 
     before do
       @comment = Comment.new context.to_h
@@ -10,14 +9,6 @@ module Comments
     def call
       context.comment = @comment
       context.fail! unless @comment.save
-    end
-
-    def action_name
-      :create
-    end
-
-    def current_user
-      context.user
     end
   end
 end
